386ccd9ea851e628dae4b6a7436c5bfe89e085d2
YU <111847207+YU000jp@users.noreply.github.com>
2023-10-22 19:05:51 +0900

    feat(Translations): full ( af, de, es, fr, id, it, ko, nb-NO, nl, pl, pt-BR, pt-PT, ru, sk, tr, uk, zh-CN, zh-Hant )

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import "@logseq/libs" //https://plugins-doc.logseq.com/
-import { setup as l10nSetup, t } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
+import { setup as l10nSetup } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
 import ja from "./translations/ja.json"
+import af from "./translations/af.json"
+import de from "./translations/de.json"
+import es from "./translations/es.json"
+import fr from "./translations/fr.json"
+import id from "./translations/id.json"
+import it from "./translations/it.json"
+import ko from "./translations/ko.json"
+import nbNO from "./translations/nb-NO.json"
+import nl from "./translations/nl.json"
+import pl from "./translations/pl.json"
+import ptBR from "./translations/pt-BR.json"
+import ptPT from "./translations/pt-PT.json"
+import ru from "./translations/ru.json"
+import sk from "./translations/sk.json"
+import tr from "./translations/tr.json"
+import uk from "./translations/uk.json"
+import zhCN from "./translations/zh-CN.json"
+import zhHant from "./translations/zh-Hant.json"
+
 import { loadLegacyDateFormatRedirect } from "./redirect"
 import { loadLegacyDateFormatReplace } from "./replace"
 import { settingsTemplate } from "./settings"
@@ -8,7 +27,11 @@ import { loadDateFormatDemo } from "./demoDateFormat"
 
 /* main */
 const main = async () => {
-  await l10nSetup({ builtinTranslations: { ja } })
+  await l10nSetup({
+    builtinTranslations: {//Full translations
+      ja, af, de, es, fr, id, it, ko, "nb-NO": nbNO, nl, pl, "pt-BR": ptBR, "pt-PT": ptPT, ru, sk, tr, uk, "zh-CN": zhCN, "zh-Hant": zhHant
+    }
+  })
   /* user settings */
   logseq.useSettingsSchema(settingsTemplate())
   if (!logseq.settings!.firstLoading)
